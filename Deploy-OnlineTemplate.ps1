8a770fa92675f3abd9014d80c640ea656bb430cb
Mathieu Quienne <Mathieu Quienne>
2017-04-27 15:02:23 +0200

    ajout du fichier param VM

--- Deploy-OnlineTemplate.ps1
+++ Deploy-OnlineTemplate.ps1
@@ -13,7 +13,7 @@ $TemplateParameterFile ="https://raw.githubusercontent.com/mathuieu/Azure-Templa
 Get-AzureRmResourceGroup -Name $ResourceGroupName -ev notPresent -ea 0
 if($notPresent){New-AzureRmResourceGroup -Name $ResourceGroupName -Location $location}
 
-Clear-Host;Test-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateUri $TemplateFile -Verbose # -TemplateParameterUri $TemplateParameterFile
+Clear-Host;Test-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateUri $TemplateFile -Verbose -TemplateParameterUri $TemplateParameterFile
 # Get-AzureRMLog -CorrelationId 15e7282b-5f90-4b02-b45b-c94665f5d885 -DetailedOutput
 
 # New-AzureRmResourceGroupDeployment -Name "Deploy-Powershell" -Mode Complete -ResourceGroupName $ResourceGroupName -TemplateUri $TemplateFile -Verbose -Force -TemplateParameterUri $TemplateParameterFile
