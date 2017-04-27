bbad6b9bd37e163b23d41902880604219ba39506
Mathieu Quienne <Mathieu Quienne>
2017-04-27 13:52:48 +0200

    parentese

--- Deploy-OnlineTemplate.ps1
+++ Deploy-OnlineTemplate.ps1
@@ -6,14 +6,14 @@
 $ResourceGroupName = "cascadejson"
 $location = "West Europe"
 
-$TemplateFile = "https://raw.githubusercontent.com/mathuieu/Azure-Templates/Cascade/vnet.json"
-$TemplateParameterFile ="https://raw.githubusercontent.com/mathuieu/Azure-Templates/Cascade/vnet.param.json"
+$TemplateFile = "https://github.com/mathuieu/Azure-Templates/raw/master/Cascade/VM-Windows.json"
+$TemplateParameterFile ="https://raw.githubusercontent.com/mathuieu/Azure-Templates/Cascade/VM-Windows.param.json"
 
 # création du ressource group destination si besoin
 Get-AzureRmResourceGroup -Name $ResourceGroupName -ev notPresent -ea 0
 if($notPresent){New-AzureRmResourceGroup -Name $ResourceGroupName -Location $location}
 
-Clear-Host;Test-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateUri $TemplateFile -Verbose -TemplateParameterUri $TemplateParameterFile
+Clear-Host;Test-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateUri $TemplateFile -Verbose # -TemplateParameterUri $TemplateParameterFile
 # Get-AzureRMLog -CorrelationId 15e7282b-5f90-4b02-b45b-c94665f5d885 -DetailedOutput
 
 # New-AzureRmResourceGroupDeployment -Name "Deploy-Powershell" -Mode Complete -ResourceGroupName $ResourceGroupName -TemplateUri $TemplateFile -Verbose -Force -TemplateParameterUri $TemplateParameterFile
